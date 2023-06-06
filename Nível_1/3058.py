ffccdf3249b91e62894ba20f0a68690b7a4ac512
Augusto <ampiatto@hotmail.com>
2023-06-06 15:33:11 -0300

    monta teste com varios inputs

--- Nível_1/3058.py
+++ Nível_1/3058.py
@@ -8,7 +8,7 @@ def supermercado():
             break
 
         mais_barato = 10000
-        for supermercado in range(0, qtd_supermercados):
+        for _ in range(0, qtd_supermercados):
             valores = [t for t in input().split(" ")]
             preco = float(valores[0])
             gramas = float(valores[1])
@@ -24,14 +24,19 @@ def supermercado():
 
         print(resultado_decimal)
 
+        #: somente para o teste
+        return resultado_decimal
 
+
+# fonte: https://pavolkutaj.medium.com/simulating-single-and-multiple-inputs-using-pytest-and-monkeypatch-6968274f7eb9
 @pytest.mark.parametrize("input, expected", [
-    ("3 3.0 100 2.0 100 5.0 100", "20.00"),
-    ("4 100.00 500 190.00 1000 200.00 900 110.00 550", "190.00"),
-    ("5 46.50 794 25.72 130 66.00 800 22.45 110 38.99 453", "58.56")
+    (["3", "3.0 100", "2.0 100", "5.0 100"], "20.00"),
+    (["4", "100.00 500", "190.00 1000", "200.00 900", "110.00 550"], "190.00"),
+    (["5", "46.50 794", "25.72 130", "66.00 800", "22.45 110", "38.99 453"], "58.56")
 ])
 def test(input, expected, monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda: input)
+    inputs = iter(input)
+    monkeypatch.setattr('builtins.input', lambda: next(inputs))
     result = supermercado()
 
     assert result == expected
