bf3fe16248763a38758b89beff7c57be266e7c25
Augusto <ampiatto@hotmail.com>
2023-06-06 22:59:50 -0300

    teste 1793

--- Nível_1/1793.py
+++ Nível_1/1793.py
@@ -1,17 +1,18 @@
-# import pytest
+import pytest
 
 def escada_rolante():
+    teste_array_num = []
     while True:
         qtd_pessoas = int(input())
         if not qtd_pessoas:
-            break
+            return " ".join(teste_array_num)
 
         t_array = [int(t) for t in input().split(" ")]
 
+        tempo_total = 10
         if qtd_pessoas == 1:
-            print(10)
+            print(tempo_total)
         else:
-            tempo_total = 10
             for idx in range(0, len(t_array) - 1):
                 if t_array[idx + 1] - t_array[idx] >= 10:
                     tempo_total += 10
@@ -19,10 +20,17 @@ def escada_rolante():
                     tempo_total += (t_array[idx + 1] - t_array[idx])
                 
             print(tempo_total)
+        
+        #: somente para o teste
+        teste_array_num.append(str(tempo_total))
 
-# TODO
 
-# pytest.mark.parametrize("input, output", [(["3 2"], "Total: R$ 10.00")])
-# @parametrize(input, output)
-# def test:
-#     escada_rolante()
+@pytest.mark.parametrize("input, expected", [
+    (["1", "5", "2", "12 25", "2", "13 16", "5", "15 20 29 31 50", "0"], "10 20 13 36")
+])
+def test(input, expected, monkeypatch):
+    inputs = iter(input)
+    monkeypatch.setattr('builtins.input', lambda: next(inputs))
+    result = escada_rolante()
+
+    assert result == expected
